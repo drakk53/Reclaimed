297682b79493d4203fa481f2a96de6a8001002eb
Haydn Trigg <me@haydntrigg.com>
2018-03-27 17:53:39 +1030

    ms30.dll

--- Reclaimed/Source/main/main.cpp
+++ Reclaimed/Source/main/main.cpp
@@ -108,28 +108,30 @@ namespace blam
 
 }
 
-
-
 BOOL APIENTRY DllMain(HMODULE, DWORD reason, LPVOID)
-
 {
-
     switch (reason)
-
     {
-
     case DLL_PROCESS_ATTACH:
-
+		return TRUE; // TODO: Start here
+	case DLL_PROCESS_DETACH:
+		return TRUE; // TODO: End here
     case DLL_THREAD_ATTACH:
-
     case DLL_THREAD_DETACH:
-
-    case DLL_PROCESS_DETACH:
-
-        break;
-
+		return TRUE;
     }
 
-    return TRUE;
+    return FALSE;
+}
 
+#include <cstdio>
+
+/*
+Required to meet the requirements for the custom DLL import
+of the ms30.dll. This could be removed from the executable
+but it can provide a useful point of reference when debugging
+*/
+extern "C" __declspec(dllexport) int ms30() {
+	printf("ms30%f", 1.0f);
+	return 1;
 }

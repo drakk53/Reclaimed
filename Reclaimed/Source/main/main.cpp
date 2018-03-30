f1b94408999a179d94443b524748b187d2414e28
Camden Smallwood <camden.smallwood@gmail.com>
2018-03-30 10:54:38 -0400

    Revert "Cleanup"
    This reverts commit e062989772215a7c84efcc17263e293ff95786dc.

--- Reclaimed/Source/main/main.cpp
+++ Reclaimed/Source/main/main.cpp
@@ -4,11 +4,46 @@
 
 #include "cseries\cseries.hpp"
 #include "memory\patching.hpp"
-#include "memory\memory.hpp"
 #include "main.hpp"
 
 namespace blam
 {
+	void *module_get_address(const dword offset)
+	{
+		static void *base_address = nullptr;
+
+		if (base_address == nullptr)
+		{
+			auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
+
+			if (snapshot == INVALID_HANDLE_VALUE)
+				return nullptr;
+
+			MODULEENTRY32 module_entry;
+			module_entry.dwSize = sizeof(MODULEENTRY32);
+
+			auto result = Module32First(snapshot, &module_entry);
+			CloseHandle(snapshot);
+
+			base_address = (result != 0) ? (void *)module_entry.modBaseAddr : nullptr;
+		}
+
+		return (char *)base_address + offset;
+	}
+
+	void unprotect_memory()
+	{
+		MEMORY_BASIC_INFORMATION MemInfo;
+
+		for (auto offset = 0; VirtualQuery(module_get_address(offset), &MemInfo, sizeof(MEMORY_BASIC_INFORMATION));)
+		{
+			offset += MemInfo.RegionSize;
+
+			if (MemInfo.Protect == PAGE_EXECUTE_READ)
+				VirtualProtect(MemInfo.BaseAddress, MemInfo.RegionSize, PAGE_EXECUTE_READWRITE, &MemInfo.Protect);
+		}
+	}
+
 	bool map_file_validation(void *)
 	{
 		return true;
@@ -31,17 +66,17 @@ namespace blam
 	bool apply_core_patches()
 	{
 		// disable tag checksums
-		if (patch_call(module_get_address(0x8392D), map_file_validation)) return true;
-		if (patch_memory(module_get_address(0x83CC1), "\x90\x90", 2)) return true;
-		if (patch_memory(module_get_address(0x847A9), "\x90\x90", 2)) return true;
+		if (!patch_call(module_get_address(0x8392D), map_file_validation)) return false;
+		if (!patch_memory(module_get_address(0x83CC1), "\x90\x90", 2)) return false;
+		if (!patch_memory(module_get_address(0x847A9), "\x90\x90", 2)) return false;
 
 		// disable preferences checksum
-		if (patch_memory(module_get_address(0x9FAF8), "\x90\x90", 2)) return true;
+		if (!patch_memory(module_get_address(0x9FAF8), "\x90\x90", 2)) return false;
 
 		// hook the game_disposing function
-		if (patch_call(module_get_address(0x150F), game_disposing)) return true;
+		if (!patch_call(module_get_address(0x150F), game_disposing)) return false;
 
-		return false;
+		return true;
 	}
 
 	bool game_attach(HMODULE module)
@@ -51,14 +86,14 @@ namespace blam
 
 		unprotect_memory();
 
-		if (apply_core_patches()) return true;
+		if (!apply_core_patches()) return false;
 
-		return false;
+		return true;
 	}
 
 	bool game_detach()
 	{
-		return false;
+		return true;
 	}
 }
 
@@ -70,12 +105,12 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID)
 	switch (reason)
 	{
 	case DLL_PROCESS_ATTACH:
-		if (blam::game_attach(module))
+		if (!blam::game_attach(module))
 			return FALSE;
 		return true;
 
 	case DLL_PROCESS_DETACH:
-		if (blam::game_detach())
+		if (!blam::game_detach())
 			return FALSE;
 		return TRUE;
 
